7a66820a244690e2f74f3de15d269e04eb05969e
Tom Irgang <thomas@irgang.eu>
2023-04-16 20:13:37 +0200

    nullable Referenzen, Textrepräsentation

--- rc_hip/repapp/admin.py
+++ rc_hip/repapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Organisator, Cafe, Frage, Gerät, Reparateur, Termin
+
+admin.site.register(Organisator)
+admin.site.register(Cafe)
+admin.site.register(Frage)
+admin.site.register(Gerät)
+admin.site.register(Reparateur)
+admin.site.register(Termin)
--- rc_hip/repapp/models.py
+++ rc_hip/repapp/models.py
@@ -5,15 +5,24 @@ class Cafe(models.Model):
     ort = models.CharField(max_length=200)
     datum = models.DateField()
 
+    def __str__(self):
+        return f'Repair-Café am {self.datum} (Ort: {self.ort})'
+
 
 class Reparateur(models.Model):
     name = models.CharField(max_length=200)
     mail = models.CharField(max_length=200)
 
+    def __str__(self):
+        return f'Reparatuer {self.name} (eMail: {self.mail})'
+
 
 class Organisator(models.Model):
     name = models.CharField(max_length=200)
     mail = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'Organisator {self.name} (eMail: {self.mail})'
 
 
 class Gerät(models.Model):
@@ -24,19 +33,30 @@ class Gerät(models.Model):
     fehler = models.TextField()
     folgetermin = models.BooleanField()
 
+    def __str__(self):
+        return f'Gerät {self.gerät} von {self.besitzer} (eMail: {self.mail})'
+
 
 class Termin(models.Model):
     uhrzeit = models.TimeField()
     bestätigt = models.BooleanField()
     cafeid = models.ForeignKey(Cafe, on_delete=models.CASCADE)
     reparateurid = models.ForeignKey(Reparateur, on_delete=models.CASCADE)
-    gerätid = models.ForeignKey(Gerät, on_delete=models.CASCADE)
+    gerätid = models.ForeignKey(Gerät, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f'Termin {self.cafeid.datum} {self.uhrzeit} für Gerät {self.gerätid.gerät} von {self.gerätid.besitzer}'
 
 
 class Frage(models.Model):
     frage = models.TextField()
     antwort = models.TextField()
     datum = models.DateField()
-    organisatorid = models.ForeignKey(Organisator, on_delete=models.CASCADE)
-    reparateurid = models.ForeignKey(Reparateur, on_delete=models.CASCADE)
+    organisatorid = models.ForeignKey(
+        Organisator, on_delete=models.CASCADE, null=True)
+    reparateurid = models.ForeignKey(
+        Reparateur, on_delete=models.CASCADE, null=True)
     gerätid = models.ForeignKey(Gerät, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Frage vom {self.datum} zum Gerät {self.gerätid.gerät}'
