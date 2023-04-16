af0c79f51675ed8132677d02cc2935e3a777ece2
Tom Irgang <thomas@irgang.eu>
2023-04-16 13:01:31 +0200

    Django Projekt

---
+++ rc_hip/rc_hip/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for rc_hip project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rc_hip.settings')
+
+application = get_asgi_application()
