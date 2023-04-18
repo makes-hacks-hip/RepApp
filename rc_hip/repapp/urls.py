92354201af65eecc18ee46716ff765cca9ec6df0
Tom Irgang <thomas@irgang.eu>
2023-04-18 21:13:30 +0200

    register device form and view

--- rc_hip/repapp/urls.py
+++ rc_hip/repapp/urls.py
@@ -1,7 +1,11 @@
+"""
+Urls of RepApp.
+"""
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path("<int:cafe_id>/", views.register_device, name="register_device"),
 ]
--- rc_hip/repapp/views.py
+++ rc_hip/repapp/views.py
@@ -1,15 +1,37 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+"""
+Views of RepApp.
+"""
+import datetime
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic
-from .models import Cafe
-
-
-def index(request):
-    return HttpResponse("Hello from RepApp.")
+from .models import Cafe, Guest, Device
 
 
 class IndexView(generic.ListView):
+    """
+    The IndexView lists all future Repair-Cafés.
+    """
     template_name = "repapp/index.html"
 
     def get_queryset(self):
-        return Cafe.objects.all()
+        return Cafe.objects.filter(event_date__gte=datetime.date.today())
+
+
+def register_device(request, cafe_id):
+    if request.method == "POST":
+        mail = request.POST.get('email_address', '')
+        device = request.POST.get('device', '')
+        issue = request.POST.get('issue', ''),
+        follow_up = request.POST.get('follow_up', 'false') == 'on'
+        return HttpResponse(f"{mail} {device} {issue} {follow_up}")
+        # return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    else:
+        cafe = get_object_or_404(Cafe, pk=cafe_id)
+        return render(
+            request,
+            "repapp/register_device.html",
+            {
+                "cafe": cafe,
+            },
+        )
