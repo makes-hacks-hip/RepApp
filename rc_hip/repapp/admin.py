c74e09585ff378f17a3bbed4afccf7e540e516d0
Tom Irgang <thomas@irgang.eu>
2023-04-24 08:20:59 +0200

    data export in admin

--- rc_hip/repapp/admin.py
+++ rc_hip/repapp/admin.py
@@ -2,20 +2,72 @@
 Admin interface configuration for RepApp.
 """
 from django.contrib import admin
-from import_export.admin import ImportExportModelAdmin
+from import_export.admin import ImportExportModelAdmin, ExportActionMixin
+from import_export import resources
 from .models import (Organisator, Cafe, Question, Device,
                      Reparateur, Appointment, Guest, Candidate)
 
 
-class DeviceAdmin(ImportExportModelAdmin):
-    resource_classes = [Device]
+class GuestResource(resources.ModelResource):
+    class Meta:
+        model = Guest
+
+    @staticmethod
+    def get_display_name():
+        return "Gäste"
+
+
+class DeviceResource(resources.ModelResource):
+    class Meta:
+        model = Device
+
+    @staticmethod
+    def get_display_name():
+        return "Geräte"
+
+
+class QuestionResource(resources.ModelResource):
+    class Meta:
+        model = Question
+
+    @staticmethod
+    def get_display_name():
+        return "Fragen"
+
+
+class AppointmentResource(resources.ModelResource):
+    class Meta:
+        model = Appointment
+
+    @staticmethod
+    def get_display_name():
+        return "Termine"
+
+
+class CandidateResource(resources.ModelResource):
+    class Meta:
+        model = Candidate
+
+    @staticmethod
+    def get_display_name():
+        return "Kandidaten"
+
+
+class ExportAdmin(ImportExportModelAdmin, ExportActionMixin):
+    resource_classes = [
+        DeviceResource,
+        GuestResource,
+        QuestionResource,
+        AppointmentResource,
+        CandidateResource,
+    ]
 
 
 admin.site.register(Organisator)
 admin.site.register(Cafe)
-admin.site.register(Question)
-admin.site.register(Guest)
-admin.site.register(Device, DeviceAdmin)
+admin.site.register(Question, ExportAdmin)
+admin.site.register(Guest, ExportAdmin)
+admin.site.register(Device, ExportAdmin)
 admin.site.register(Reparateur)
-admin.site.register(Appointment)
-admin.site.register(Candidate)
+admin.site.register(Appointment, ExportAdmin)
+admin.site.register(Candidate, ExportAdmin)
